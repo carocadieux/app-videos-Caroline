df0a5b1284f7fb2b638861f0de3881347961f35b
carocadieux <caroline-cadieux@outlook.com>
2023-11-04 11:47:46 -0400

    ajout de css

--- src/app/mock-videos.ts
+++ src/app/mock-videos.ts
@@ -2,25 +2,25 @@ import { Video } from "./video";
 
 export const VIDEOS: Video[] = [
     {
-    url_image:'https://picsum.photos/seed/picsum/200/300', 
-    sous_titres: ['st'],
-    nom: 'Halloween',
-    description: 'lorem',
+    url_image:'https://usercontent.one/wp/www.screentune.com/wp-content/uploads/2018/12/MV5BNTAzOTYxMjEtMGNhNC00NGJjLTk4NWQtNWM0M2RjZTU5Njc0XkEyXkFqcGdeQXVyNzA4NDgwNTI@._V1_.jpg?media=1649161309', 
+    sous_titres: ['cc'],
+    nom: "Halloween",
+    description: "La nuit d'Halloween 1963. Le jeune Michael Myers se précipite dans la chambre de sa soeur aînée et la poignarde sauvagement. Après son geste, Michael se mure dans le silence et est interné dans un asile psychiatrique. Quinze ans plus tard, il s'échappe de l'hôpital et retourne sur les lieux de son crime. Il s'en prend alors aux adolescents de la ville.",
     code: '',
-    score_video: 235,
+    score_video: 7025,
     categories: ['Horreur'],
     auteur: {
-        nom: 'John Carpenter',
-        description: 'lorem',
+        nom: "John Carpenter",
+        description: "John Carpenter est un réalisateur, scénariste, producteur et compositeur américain né le 16 janvier 1948 à Carthage, dans l'État de New York.",
         verifier: 'true' ,
     },
     datePublication: new Date(1978, 10, 25),
     duree: 1.42,
-    nombreVues: 0,
+    nombreVues: 2000,
     avis: [
             {
-                note: 0,
-                commentaires: 'lorem',
+                note: 4,
+                commentaires: "Très bon film classique horreur",
             }
         ]
     
